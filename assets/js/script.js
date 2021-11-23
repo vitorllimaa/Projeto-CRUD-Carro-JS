91e62749fb16555484bcd6bbc79b989dcb847868
Your Name <vitorlimavital@hotmail.com>
2021-11-22 21:44:33 -0300

    pop-up do template

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,33 @@ function allList() {
 list.forEach((item) => {
     item.addEventListener('mouseover', allList)
 });
+
+
+
+const editCotainer = document.querySelector('.edit-cotainer');
+const deleteCotainer = document.querySelector('.edit-coitainer_header > ion-icon')
+const iconEdit = document.querySelectorAll('td > ion-icon');
+const registerBtn = document.querySelector('.btn');
+
+iconEdit.forEach(e => {
+    e.addEventListener('click', value => {
+        if(value.target.name === 'create-outline') {
+            document.querySelector('.edit-coitainer_header > h2').textContent = 'Editar Cadastro';
+    document.querySelector('form > button').textContent = 'Editar';
+            editCotainer.classList.add('show');
+        } else{
+            confirm('Tem certeza que deseja Excluir o pedido?');
+        }
+    })
+})
+
+
+deleteCotainer.addEventListener('click', e => {
+    editCotainer.classList.remove('show');
+})
+
+registerBtn.addEventListener('click', e => {
+    document.querySelector('.edit-coitainer_header > h2').textContent = 'Cadastrar';
+    document.querySelector('form > button').textContent = 'Cadastrar';
+    editCotainer.classList.add('show');
+})
